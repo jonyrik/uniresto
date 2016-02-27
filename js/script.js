baa78496af82b5b19826e83249e1d972afb58332
Jonathan <jonyri@gmail.com>
2016-02-27 15:10:45 -0300

    05 style, restos, arreglo script

--- js/script.js
+++ js/script.js
@@ -1,32 +1,5 @@
 $(document).ready(function() {
 
-// Amigos
-  var btn_filtros = $('#btn-filtros');
-  var btn_filtros2 = $('#btn-filtros2');
-  var cont_select = $('.cont-select');
-
-  $(btn_filtros).click(function () {
- 	$(cont_select).css('display', 'table');
-      $(btn_filtros2).css('display', 'table');
-      $(this).css('display', 'none');
- });
-
-  $(btn_filtros2).click(function () {
- 	$(cont_select).css('display', 'none');
-      $(btn_filtros).css('display', 'table');
-      $(this).css('display', 'none');
- });
-
-  $(enviar).click(function () {
-     $(enviada).css('display', 'block');
-      $(this).css('display', 'none');
- });
- $(enviada).click(function () {
-     $(enviar).css('display', 'block');
-      $(this).css('display', 'none');
- });
-
-
   var btn_demora1 = $('#btn-demora1');
     var btn_demora2 = $('#btn-demora2');
     var btn_demora3 = $('#btn-demora3');
